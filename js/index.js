00fe6174c0dda1580aee4c44927c5b517a33484d
Luana <santos.luanabeatriz1995@gmail.com>
2023-05-28 11:17:42 +0100

    Adicionando formulários

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const dia = document.getElementById("dia")
 const hora = document.getElementById("hora")
 const minuto = document.getElementById("minuto")
 const segundo = document.getElementById("segundo")
+let ingressos = [];
 
 const lancamento = "01 dec 2023"
 
@@ -28,3 +29,33 @@ function formatoTempo( tempo ){
 
 countDown();
 setInterval(countDown, 1000);
+
+function highlightCard(selector){
+    let element = document.querySelector(selector);
+
+    element.classList.toggle("card-highlight");
+}
+
+function selectCard(selector){
+    let element = document.querySelector(selector);
+    element.classList.toggle("card-selected");
+    if(ingressos.includes(selector)){
+        index = ingressos.indexOf(selector);
+        ingressos.splice(index, 1);
+    }else{
+        ingressos.push(selector);
+    }
+}
+
+function showSelectedCards(){
+    let myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+    let modalBody = document.querySelector(".modal-body");
+    
+    if(ingressos.length > 0){
+        modalBody.innerHTML = ingressos;
+    }else{
+        modalBody.innerHTML = "Nenhum ingresso selecionado";
+    }
+
+    myModal.show();
+}
